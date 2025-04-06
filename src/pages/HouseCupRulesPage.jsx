65ab734e27a282165169062c3ce564e376b814cc
SereMark <seremark056@gmail.com>
2025-04-06 04:03:23 +0200

    fix: Delay closing Table of Contents for better user experience

--- src/pages/HouseCupRulesPage.jsx
+++ src/pages/HouseCupRulesPage.jsx
@@ -21,7 +21,10 @@ function HouseCupRulesPage() {
       const yOffset = -80;
       const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
       window.scrollTo({ top: y, behavior: "smooth" });
-      setShowTOC(false);
+      
+      setTimeout(() => {
+        setShowTOC(false);
+      }, 600);
     }
   };
 
