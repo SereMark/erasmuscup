f8fb88779775e131c16e6834b12dd140c3afeaf3
SereMark <seremark056@gmail.com>
2025-04-07 16:07:45 +0200

    fix: Correct scoreboard data values for accuracy

--- src/constants/scoreboardData.js
+++ src/constants/scoreboardData.js
@@ -6,8 +6,8 @@ export const SCOREBOARD_DATA = [
   { id: 2, date: "28.03.2025", event: "Petanque", theHoo: 27, brewCrew: 76, redStorm: 55, deepJungle: 102 },
   { id: 3, date: "05.04.2025", event: "Beer pong", theHoo: 47, brewCrew: 30, redStorm: 14, deepJungle: 61 },
   { id: 4, adjustment: "Public Nudity", theHoo: 30, brewCrew: null, redStorm: 30, deepJungle: null },
-  { id: 5, adjustment: "Buffaloes", theHoo: null, brewCrew: -10, redStorm: -10, deepJungle: -20 },
-  { id: 6, total: true, event: "Total", theHoo: 154, brewCrew: 171, redStorm: 114, deepJungle: 243 }
+  { id: 5, adjustment: "Buffaloes", theHoo: null, brewCrew: -20, redStorm: -10, deepJungle: -20 },
+  { id: 6, total: true, event: "Total", theHoo: 154, brewCrew: 161, redStorm: 114, deepJungle: 243 }
 ];
 
 // Constants
